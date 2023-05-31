b17b8c813522ab62d55e9b161bbc839dcf77ea30
Ryan Taylor <ryan.taylor1@yahoo.com>
2023-05-31 12:49:35 -0700

    added h1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,8 @@ import SearchBar from '../SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
-      <SearchBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jammming Spotify Playlist</h1>
       </header>
     </div>
   );
